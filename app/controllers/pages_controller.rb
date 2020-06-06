2a7849d9d20573f471210d6b3c9151fd1930bcdf
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-06 23:42:01 +0000

    Add the contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
     end 
      def about 
      end 
+
+     def contact 
+     end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get '/portfolio' => 'pages#portfolio'
   get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
 end
